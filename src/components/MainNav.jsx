f0583655f7f1914475f379b14e31a346312b049e
Carla Borges Reis <carlareis.dev.com>
2023-11-06 09:13:59 -0800

    deleted querychange form mainnnav

--- src/components/MainNav.jsx
+++ src/components/MainNav.jsx
@@ -1,24 +1,16 @@
 import { NavLink } from "react-router-dom";
-import PropTypes from 'prop-types';
 
-const MainNav = ({ changeQuery }) => {
-  const handleClick = (e) => {
-    changeQuery(e.target.innerText)
-  }
+const MainNav = () => {
 
   return (
     <nav className="main-nav">
         <ul>
-          <li><NavLink to='/fish' onClick={(e) => handleClick(e)} >Fish</NavLink></li>
-          <li><NavLink to='/birds' onClick={(e) => handleClick(e)} >Birds</NavLink></li>
-          <li><NavLink to='/flowers' onClick={(e) => handleClick(e)} >Flowers</NavLink></li>
+          <li><NavLink to='/fish' >Fish</NavLink></li>
+          <li><NavLink to='/birds' >Birds</NavLink></li>
+          <li><NavLink to='/flowers' >Flowers</NavLink></li>
         </ul>
       </nav>
   )
   }
 
-  MainNav.propTypes = {
-    changeQuery: PropTypes.func
-  };
-
-export default MainNav
+export default MainNav;
