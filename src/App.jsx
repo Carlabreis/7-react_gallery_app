1ebaaf5c68eced7726b6847aa3990d02162ae0f2
Carla Borges Reis <carlareis.dev.com>
2023-11-05 21:01:46 -0800

    edited some messages and cleaned up code

--- src/App.jsx
+++ src/App.jsx
@@ -3,57 +3,55 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
 import apiKey from "./config";
 
-// App components
+/* IMPORT APP COMPONENTS */
 import SearchForm from "./components/SearchForm";
 import MainNav from "./components/MainNav";
 import PhotoList from "./components/PhotoList";
 import PageNotFound from "./components/PageNotFound";
 
 function App() {
+
+  /* HOOKS */
   const [photos, setPhotos] = useState([]);
-  const [query, setQuery] = useState("mountains");
+  const [query, setQuery] = useState("birds");
   const [loading, setLoading] = useState(true);
+
 
   useEffect(() => {
     setLoading(true);
-    let activeFetch = true;
 
     fetch(
       `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`
     )
       .then((response) => response.json())
       .then((responseData) => {
-        if (activeFetch) {
-          console.log(responseData.photos.photo);
-          setPhotos(responseData.photos.photo);
-          setLoading(false);
-        }
+        setPhotos(responseData.photos.photo);
+        {/* Stop loading once the data is received and set to photos */}
+        setLoading(false);
       })
       .catch((err) => console.log("Error fetching and parsing data", err));
-    return () => {
-      activeFetch = false;
-    };
   }, [query]);
+
 
   const handleQueryChange = (searchInput) => {
     setQuery(searchInput);
   };
 
+
   return (
     <div className="container">
+
+      {/* Fixed components */}
       <SearchForm changeQuery={handleQueryChange} />
       <MainNav changeQuery={handleQueryChange} />
+
+      {/* on loading, then render routes once data received */}
       {loading ? (
-        <p>Loading...</p>
+        <h3>Loading...</h3>
       ) : (
         <Routes>
-          <Route path="/" element={<Navigate replace to="/mountains" />} />
-          <Route
-            path="/:query"
-            element={
-              <PhotoList data={photos} changeQuery={handleQueryChange} />
-            }
-          />
+          <Route path="/" element={<Navigate replace to="/birds" />} />
+          <Route path="/:query" element={<PhotoList data={photos} changeQuery={handleQueryChange} />} />
           <Route path="/*" element={<PageNotFound />} />
         </Routes>
       )}
